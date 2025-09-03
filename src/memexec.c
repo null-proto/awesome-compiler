29fd2f05692cde52b59f5826cf28f3260ebc8b23
null-proto <209804848+null-proto@users.noreply.github.com>
2025-09-03 18:06:31 +0530

    update memexec

--- src/memexec.c
+++ src/memexec.c
@@ -10,6 +10,8 @@ int main() {
 
   size_t d = 11;
 
+	// request memory from kernel
+	// with READ + WRITE
   void *mapped_memory =
       mmap(NULL, d, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
@@ -18,25 +20,24 @@ int main() {
     return 1;
   }
 
-  // write
-  //
+  // sample instruction that just return `33`
+	//
   unsigned char code[11] = {
       0x48, 0xB8, 0x21, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov rax, 33
       0xC3                                                        // ret
   };
 
+
 	memcpy(mapped_memory,code,11);
 
-  // change permission
+  // change permission to READ + EXECUTE
   //
   if (mprotect(mapped_memory, d, PROT_READ | PROT_EXEC) == -1) {
     printf("failed to change mod\n");
     return 1;
   }
 
-	int (*go)() =  mapped_memory;
-
-	int res = go();
+	int res = ((int(*)()) mapped_memory)();  // call the array
 
 	printf("res = %d" , res);
 
